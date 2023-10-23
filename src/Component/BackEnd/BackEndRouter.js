d6c6322026859e87df5c5e010f1f71e0be691450
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-23 19:02:26 +0530

    add new

--- src/Component/BackEnd/BackEndRouter.js
+++ src/Component/BackEnd/BackEndRouter.js
@@ -1,36 +1,28 @@
-import React, { useEffect } from 'react'
-
-import { Route, Routes, Navigate } from 'react-router-dom'
-import ProtectedRoute from './ProtectedRoute';
+import React, { useEffect } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import BackHome from './BackHome';
-
 import NotFoundPage from './NotFound';
 import BackendDashboard from './BackendDashboard';
 import BackHomePage from './BackHomePage';
-const isAuthenticated = true;
+
+const isAuthenticated = true; // You can replace this with your actual authentication logic.
+
 export default function BackEndRouter() {
-
-
   useEffect(() => {
-    // Scroll to top whenever the route changes
+    // Scroll to the top whenever the route changes
     window.scrollTo(0, 0);
-  }, );
+  }, []);
 
   return (
     <div>
-
-        <Routes>          
-                 
+      <Routes>
         <Route path="/" element={<BackHome />} />
-      {isAuthenticated ? (
-        <Route path="/backEndDashboard" element={<BackendDashboard />} />
-      ) : (
-        <Navigate to="/" />
-      )}
-  
-
-    </Routes>
-
+        {isAuthenticated ? (
+          <Route path="/backEndDashboard" element={<BackendDashboard />} />
+        ) : (
+          <Navigate to="/" />
+        )}
+      </Routes>
     </div>
-  )
+  );
 }
