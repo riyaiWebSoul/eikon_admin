643ab238db4a0ac0bb17505c4d88466f686633e6
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-25 17:25:21 +0530

    condition chack

--- src/Component/BackEnd/BackAppoinmentSuccess.js
+++ src/Component/BackEnd/BackAppoinmentSuccess.js
@@ -3,36 +3,17 @@ import axios from 'axios';
 
 function BackAppointmentSuccess() {
   const [responseData, setResponseData] = useState([]);
-  const [id, setId] = useState('');
-  const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [comments, setComments] = useState('');
-  const [time, setTime] = useState('');
-  const [date, setDate] = useState('');
-  const [gender, setGender] = useState('');
-  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
   const [deleteIndex, setDeleteIndex] = useState(null);
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
 
   useEffect(() => {
     handleGet();
   }, []);
 
-  const handleGet = async () => {
-    try {
-      const response = await axios.get(`https://eikon-api.onrender.com/appointments`);
-      const data = response.data;
-      setResponseData(data);
-    } catch (error) {
-      console.error('Error making GET request:', error);
-      // Handle errors here.
-    }
-  };
-
   const openConfirmationModal = (index) => {
     setDeleteIndex(index); // Store the index to delete
     setShowConfirmationModal(true);
-  }
+  };
 
   const handleDelete = async () => {
     try {
@@ -46,8 +27,7 @@ function BackAppointmentSuccess() {
         }
 
         setDeleteIndex(null); // Clear the delete index
-        handleGet()
-  
+        handleGet();
       }
     } catch (error) {
       console.error('Error making DELETE request:', error);
@@ -55,22 +35,24 @@ function BackAppointmentSuccess() {
     }
   };
 
+  const handleGet = async () => {
+    try {
+      const response = await axios.get(`https://eikon-api.onrender.com/appointments`);
+      const data = response.data;
+      setResponseData(data);
+    } catch (error) {
+      console.error('Error making GET request:', error);
+      // Handle errors here.
+    }
+  };
+
   return (
     <div className="container">
-      <h2 className='text-center p-5'>Booked Appointments List</h2>
-      <div className="row">
-        <div className="col-sm-6">
-          <div className="btn-group">
-            <button className="btn btn-primary m-1" onClick={handleGet}>
-              GET
-            </button>
-          </div>
-        </div>
-      </div>
-      <br />
-      {responseData.length > 0 && (
-        <div>
-          <h3>Edit Data:</h3>
+      <h2 className="text-center p-5">Booked Appointments List</h2>
+      
+      {responseData.length > 0 ? ( // Check if there is data
+        <div className='text-center'>
+       
           <table className="table">
             <thead>
               <tr>
@@ -106,53 +88,54 @@ function BackAppointmentSuccess() {
             </tbody>
           </table>
         </div>
+      ) : (
+        // No data found, display a message
+        <div className="text-center mt-5">No Appointment Yet</div>
       )}
+
       <div>
         {showConfirmationModal && (
-<div>
-<div className="modal fade show" style={{ display: "block" }}>
-            <div className="modal-dialog ">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title">Confirm Delete</h5>
-                  <button
-                    type="button"
-                    className="close"
-                    onClick={() => setShowConfirmationModal(false)}
-                  >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                  Are you sure you want to delete this entry?
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    onClick={() => setShowConfirmationModal(false)}
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-danger"
-                    onClick={() => {
-                      setShowConfirmationModal(false);
-                      handleDelete();
-                    }}
-                  >
-                    Confirm Delete
-                  </button>
+          <div>
+            <div className="modal fade show" style={{ display: "block" }}>
+              <div className="modal-dialog ">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Confirm Delete</h5>
+                    <button
+                      type="button"
+                      className="close"
+                      onClick={() => setShowConfirmationModal(false)}
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                    Are you sure you want to delete this entry?
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={() => setShowConfirmationModal(false)}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      onClick={() => {
+                        setShowConfirmationModal(false);
+                        handleDelete();
+                      }}
+                    >
+                      Confirm Delete
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-
+            <div className="modal-backdrop fade show"></div>
           </div>
-        <div className="modal-backdrop fade show"></div>
-
-</div>
-          
         )}
       </div>
     </div>
--- src/Component/BackEnd/BackView.js
+++ src/Component/BackEnd/BackView.js
@@ -1,32 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
-function BackView({setModalState}) {
-
+function BackView({ setModalState }) {
   const [responseData, setResponseData] = useState([]);
-  const [id, setId] = useState('');
-  const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [comments, setComments] = useState('');
-  const [time, setTime] = useState('');
-  const [date, setDate] = useState('');
-  const [gender, setGender] = useState('');
   const [deleteIndex, setDeleteIndex] = useState(null);
   const [showConfirmationModal, setShowConfirmationModal] = useState(false);
 
-
-  const openConfirmationModal = (index) => {
-    setDeleteIndex(index); // Store the index to delete
-    setShowConfirmationModal(true);
-  }
   useEffect(() => {
     handleGet();
   }, []);
 
-
-
+  const openConfirmationModal = (index) => {
+    setDeleteIndex(index);
+    setShowConfirmationModal(true);
+  };
 
   const handleGet = async () => {
     try {
@@ -37,100 +24,46 @@ function BackView({setModalState}) {
       console.error('Error making GET request:', error);
     }
   };
-  const handleUpdate = async (index) => {
+
+  const handleUpdate = async () => {
     try {
-       handleGet()
       const updatedData = [...responseData];
       const checkId = updatedData[deleteIndex]._id;
       const checkIdString = checkId.toString();
-     
+
       const response = await axios.delete(`https://eikon-api.onrender.com/enquiry/${checkIdString}`);
       if (response.status === 200) {
-       
         updatedData.splice(deleteIndex, 1);
         setResponseData(updatedData);
-       
       }
-      handleGet()
+      handleGet();
     } catch (error) {
       console.error('Error making DELETE request:', error);
     }
   };
-
-  const handleDelete = async () => {
-    try {
-      if (deleteIndex !== null) {
-        const entryToDelete = responseData[deleteIndex];
-        const response = await axios.delete(`https://eikon-api.onrender.com/appointments/${entryToDelete._id}`);
-
-        if (response.status === 200) {
-          const updatedData = responseData.filter((entry, index) => index !== deleteIndex);
-          setResponseData(updatedData);
-        }
-
-        setDeleteIndex(null); // Clear the delete index
-        handleGet()
-  
-      }
-    } catch (error) {
-      console.error('Error making DELETE request:', error);
-      // Handle errors here.
-    }
-  };
- 
-
-  // const handleUpdate = async () => {
-  //   try {
-  //     const updatedData = [...responseData];
-  //     const index = updatedData.findIndex((entry) => entry._id === id);
-
-  //     if (index !== -1) {
-  //       updatedData[index] = {
-  //         _id: id,
-  //         name: userName,
-  //         email: email,
-  //         phone: phone,
-  //         comments: comments,
-  //         time: time,
-  //         date: date,
-  //         gender: gender,
-  //       };
-
-  //       setResponseData(updatedData);
-  //       handleGet()
-  //     }
-  //   } catch (error) {
-  //     console.error('Error making PATCH request:', error);
-  //   }
-  // };
 
   return (
     <div className="container">
-
+      {responseData.length > 0 ? ( // Check if there is data
         <div>
-      
           <table className="table mt-5">
             <thead>
               <tr>
-                <th className='text-center'>Serial Number</th>
-                <th className='text-center'>Name</th>
-                <th className='text-center'>Email</th>
-                <th className='text-center'>Phone</th>
-            
-                <th className='text-center'>Delete</th>
+                <th className="text-center">Serial Number</th>
+                <th className="text-center">Name</th>
+                <th className="text-center">Email</th>
+                <th className="text-center">Phone</th>
+                <th className="text-center">Delete</th>
               </tr>
             </thead>
             <tbody>
               {responseData.map((entry, index) => (
                 <tr key={index}>
-    
-                  <td className='text-center'>{index + 1}</td>
-                  <td className='text-center'>{entry.name}</td>
-                  <td className='text-center'>{entry.email}</td>
-                  <td className='text-center'>{entry.phone}</td>
-             
-                
-                  <td className='text-center'>
+                  <td className="text-center">{index + 1}</td>
+                  <td className="text-center">{entry.name}</td>
+                  <td className="text-center">{entry.email}</td>
+                  <td className="text-center">{entry.phone}</td>
+                  <td className="text-center">
                     <button className="btn btn-danger text-center" onClick={() => openConfirmationModal(index)}>
                       Delete
                     </button>
@@ -139,58 +72,55 @@ function BackView({setModalState}) {
               ))}
             </tbody>
           </table>
-          
         </div>
-
-
-        <div>
-        {showConfirmationModal && (
-<div>
-<div className="modal fade show" style={{ display: "block" }}>
-            <div className="modal-dialog ">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title">Confirm Delete</h5>
-                  <button
-                    type="button"
-                    className="close"
-                    onClick={() => setShowConfirmationModal(false)}
-                  >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                  Are you sure you want to delete this entry?
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    onClick={() => setShowConfirmationModal(false)}
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-danger"
-                    onClick={() => {
-                      setShowConfirmationModal(false);
-                      handleUpdate();
-                    }}
-                  >
-                    Confirm Delete
-                  </button>
-                </div>
-              </div>
-            </div>
-
+      ) : (
+        // No data found, display a message
+        <div className="text-center mt-5">No Enquiry Yet</div>
+      )}
+{showConfirmationModal && (
+  <div>
+    <div className="modal fade show" style={{ display: "block" }}>
+      <div className="modal-dialog ">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Confirm Delete</h5>
+            <button
+              type="button"
+              className="close"
+              onClick={() => setShowConfirmationModal(false)}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
-        <div className="modal-backdrop fade show"></div>
-
-</div>
-          
-        )}
+          <div className="modal-body">
+            Are you sure you want to delete this entry?
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() => setShowConfirmationModal(false)}
+            >
+              Cancel
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={() => {
+                setShowConfirmationModal(false);
+                handleUpdate();
+              }}
+            >
+              Confirm Delete
+            </button>
+          </div>
+        </div>
       </div>
+    </div>
+    <div className="modal-backdrop fade show"></div>
+  </div>
+)}
+
     </div>
   );
 }
